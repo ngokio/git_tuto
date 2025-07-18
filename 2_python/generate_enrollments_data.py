677c0055b6b9559524cff042a544d666af5af060
Christian <christianngokio@gmail.com>
2025-07-18 16:46:24 +0200

    all variable pwd

--- 2_python/generate_enrollments_data.py
+++ 2_python/generate_enrollments_data.py
@@ -12,7 +12,7 @@ DB_PORT = 5432
 
 conn = psycopg2.connect(database=DB_NAME,
                         user=DB_USER,
-                        password=DB_PASS,
+                        password=DB_PASSWORD,
                         host=DB_HOST,
                         port=DB_PORT)
 
